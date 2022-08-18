8ff0fc083b964452c1c25c2e6e2a349241b9aa6f
unknown <francomeinardo13@gmail.com>
2022-08-17 22:04:46 -0300

    .

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,6 +34,7 @@ const NavBar = () => {
 
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed z-50">
+      
       <div>
         <h1 className="text-5xl font-signature ml-2">Franco</h1>
       </div>

dc5f1f9206018c6d432a32630370d52742f94838
unknown <francomeinardo13@gmail.com>
2022-08-22 16:27:41 -0300

    .

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,41 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div
-    name="about"
-    className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
-  >
-    <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-      <div className="pb-8">
-        <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-          About
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-10">
+          👋 Hi, my name is Franco, I'm 19 years old, I live in Córdoba,
+          Argentina.
+        </p>
+
+        <p className="text-xl mt-5">
+          💻 For more than 1 year I discovered the world of programming for
+          which I invested time studying as a self-taught, developing different
+          skills such as logic and problem solving. Currently I finished an
+          intensive course of more than 1000 hours in which I learned to work in
+          a team and carry out different projects with the latest technologies.
+        </p>
+        <p className="text-xl mt-5">
+          👨‍💻 I am characterized by being organized, self-taught, responsible,
+          strict with myself and always trying to learn about new technologies,
+          since I am passionate about everything related to development. I love
+          challenges and having goals to be able to perform as a developer. When
+          there is something I don't know, I investigate until I find the
+          solution. I consider myself a good colleague and supportive. I love
+          working in a team and I always try to help others.
+        </p>
+
+        <p className="text-xl mt-5">
+          🌎 I would like to work in an environment where people are committed,
+          where I can continue to grow and where I am given the right tools to
+          project myself as a professional and grow as a person.
+        </p>
+
+        <p className="text-xl mt-5 mb-20">
+          🚀 My goal is to become a Senior Developer to be able to pass on my
+          knowledge and experience to others, which I am passionate about.
         </p>
       </div>
-
-      <p className="text-xl mt-10">
-      👋 Hola, mi nombre es Franco, tengo 19 años, vivo en Córdoba,  Argentina.
-      </p>
-
-      <p className="text-xl mt-5">
-      💻 Desde hace más de 1 año descubrí el mundo de la programación por el cual invertí tiempo estudiando como autodidacta, desarrollando diferentes habilidades como la lógica y resolución de problemas. Actualmente finalicé un curso intensivo de más 1000 horas en el cual aprendí a trabajar en equipo y realizar diferentes proyectos con las últimas tecnologías.
-
-      </p>
-      <p className="text-xl mt-5">
-      👨‍💻 Me caracterizo por ser organizado, autodidacta, responsable, estricto conmigo mismo y siempre tratando de aprender sobre nuevas tecnologías, ya que me apasiona todo lo relacionado a desarrollo. Me encantan los desafíos y tener objetivos para poder desempeñarme como developer. Cuando hay algo que no se, indago hasta encontrar la solución. Me considero buen compañero y solidario. Me encanta trabajar en equipo y trato siempre de ayudar a los demás.
-      </p>
-      
-      <p className="text-xl mt-5">
-      🌎 Me gustaría trabajar en un ambiente donde se apueste por las personas, donde pueda seguir creciendo y se me brinden las herramientas adecuadas para proyectarme como profesional  y crecer como persona. 
-      </p>
-
-
-      <p className="text-xl mt-5 mb-20">
-      🚀 Mi objetivo es llegar a ser Developer Senior para poder transmitir mis conocimientos y experiencia a los demás, lo cual me apasiona.
-      </p>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
